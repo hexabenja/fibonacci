14ec99ed16170bc0d7518afc900a200cbf0eb19b
hexabenja <benjamin.n.ign@gmail.com>
2025-08-09 23:20:42 -0400

    Agregados INT, valores numericos definidos y formulas

--- fibonacci.c
+++ fibonacci.c
@@ -1,2 +1,11 @@
-#include <studio.h>
-
+#include <stdio.h>
+int main(){
+    int n;
+    printf("Ingresa el valor numerico:");
+    scanf("%d" ,&n);
+    int a=1 ,b=1 ,sum=1; // Definir A como 1, B como 1 y SUM como 1;
+    for(int i=1;i<=(n-2);i++){
+            sum=a+b;
+    a=b
+    b=sum;
+    }

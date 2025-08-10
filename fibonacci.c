336c357caee7fe01ca5118f704a8fe1f8c31eedf
hexabenja <benjamin.n.ign@gmail.com>
2025-08-10 16:48:24 -0400

    Contador solucionado, ahora funciona correctamente. NOTA: ya que la CPU realiza las iteraciones demasiado rapido agregué un sleep(1) para agregar un segundo a las iteraciones para que sea observable el resultado de tiempo de ejecución, leyendo el número después del decimal

--- fibonacci.c
+++ fibonacci.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-#include <time.h>
+#include <time.h> // Incluir calcular tiempo
+#include <unistd.h> // Incluir Sleep
 
 int main(){
-    clock_t inicio, fin;
+
     double tiempo_cpu;
     int n; // Integrar valores numericos al sistema
     printf("Ingresa el valor numerico:"); // Printear primer mensaje
     scanf("%d", &n); // scanf lee los datos ingresados y los agrega al stack como variables
-    int a=1 ,b=1 ,sum=1; // Definir A como 1, B como 1 y SUM como 1;
+    clock_t inicio, fin;
     inicio = clock();
+    sleep(1);
+    int a=1 ,b=1 ,sum=1; // Definir A como 1, B como 1 y SUM como 1;
+
     for(int i=1;i<=(n-2);i++){ // Declara I como 1, I sera el resultado de Input substraido por 2 para luego adicionar por 1 representado por ++ para evitar bucle
 
     sum=a+b; // sum es igual a iteracion 1 , a es igual iteracion 2 y b es igual a iteracion 3
